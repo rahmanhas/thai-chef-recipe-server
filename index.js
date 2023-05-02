84aa4fb06dfeea5d8fa2f15a33dee2d446c38ee3
rahmanhas <rahmanhas@gmail.com>
2023-05-03 00:39:25 +0600

    removed unnecessary codes

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ const chefInfo = require('./data/chefinfo.json')
 const cors = require('cors')
 app.use(cors())
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Chef API')
 })
 app.get('/chefinfo', (req, res) => {
   res.send(chefInfo)
 })
-app.get('/categories', (req, res) => {
-  res.send(categories)
-})
+
 app.get('/chefinfo/:id', (req, res) => {
   const id = req.params.id;
   if (id == 0) {
-    // res.send(chefInfo)
+ 
   }
   else {
     const individualChef = chefInfo.filter(chef => chef.id == id);
